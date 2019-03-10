1d075df78758b4df9ee7e80738b86f764e25afcb
yashraikar1087 <yashraikar1087@gmail.com>
2019-03-10 15:50:34 -0600

    add express

--- npm/index.js
+++ npm/index.js
@@ -5,6 +5,13 @@ var port = process.env.PORT || 3000;
 
 app.use("/assets", express.static(__dirname + "/public"));
 
+app.set("view engine", "ejs");
+
+app.use("/", function(req, res, next) {
+  console.log(req.params.id);
+  next();
+});
+
 app.get("/person/:id", function(req, res) {
   res.send(
     "<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>" +
@@ -14,9 +21,10 @@ app.get("/person/:id", function(req, res) {
 });
 
 app.get("/", function(req, res) {
-  res.send(
-    "<html><head><link href='assets/style.css' type='text/css' rel='stylesheet' /></head><body><h1>Hello</h1></body></html>"
-  );
+  console.log();
+  res.render("index", {
+    ID: req.params.id
+  });
 });
 
 app.listen(port);
