ca20722ec1b4da2c3110257884a0c077a95f6c18
yashraikar1087 <yashraikar1087@gmail.com>
2019-03-10 12:19:25 -0600

    add middleware

--- npm/index.js
+++ npm/index.js
@@ -3,9 +3,19 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use("/assets", express.static(__dirname + "/public"));
+
 app.get("/person/:id", function(req, res) {
   res.send(
-    "<html><head></head><body><h1>" + req.params.id + "</h1></body></html>"
+    "<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>" +
+      req.params.id +
+      "</h1></body></html>"
+  );
+});
+
+app.get("/", function(req, res) {
+  res.send(
+    "<html><head><link href='assets/style.css' type='text/css' rel='stylesheet' /></head><body><h1>Hello</h1></body></html>"
   );
 });
 
